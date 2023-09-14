a6644acd74ccaf96aac85dcdf43261fbb3c91d6c
cocoa00 <834237195@qq.com>
2023-09-14 16:49:33 +0800

    增加了登陆页面前端显示

--- web/src/components/NavBar.vue
+++ web/src/components/NavBar.vue
@@ -18,17 +18,31 @@
         </li>
       </ul>
 
-      <ul class="navbar-nav">
+      <ul class="navbar-nav" v-if="$store.state.user.is_login">
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            lc
+            {{$store.state.user.username}}
           </a>
           <ul class="dropdown-menu">
             <li><router-link class="dropdown-item" :to="{name: 'user_bots_index'}">我的bots</router-link></li>
-            <li><a class="dropdown-item" href="#">退出</a></li>
+            <li><a class="dropdown-item" href="#" @click="logout">退出</a></li>
           </ul>
         </li>
       </ul>
+
+      <ul class="navbar-nav" v-else>
+        <li class="nav-item">
+          <router-link class="nav-link" :to="{name: 'user_account_login'}" role="button">
+            登录
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" :to="{name: 'user_account_register'}" role="button">
+            注册
+          </router-link>
+        </li>
+      </ul>
+
     </div>
   </div>
 </nav>
@@ -37,12 +51,19 @@
 <script>
 import {useRoute} from 'vue-router'
 import { computed } from 'vue';
+import { useStore } from 'vuex';
 export default {
   setup() {
+    const store = useStore();
     const route = useRoute();
     let route_name = computed(() => route.name);
+
+    const logout = () => {
+      store.dispatch('logout');
+    }
     return {
-      route_name
+      route_name,
+      logout
     }
   }
 }
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -4,6 +4,9 @@ import RecordIndexView from '../views/record/RecordIndexView.vue'
 import RanklistIndexView from '../views/ranklist/RanklistIndexView.vue'
 import UserBotsIndexView from '../views/user/bots/UserBotsIndexView.vue'
 import NotFound from '../views/error/NotFound.vue'
+import UserAccountLoginView from '../views/user/account/UserAccountLoginView.vue'
+import UserAccountRegisterView from '../views/user/account/UserAccountRegisterView.vue'
+
 
 const routes = [
   {
@@ -30,6 +33,16 @@ const routes = [
     path: '/user/bots/',
     name: 'user_bots_index',
     component: UserBotsIndexView,
+  },
+  {
+    path: '/user/account/login/',
+    name: 'user_account_login',
+    component: UserAccountLoginView,
+  },
+  {
+    path: '/user/account/register',
+    name: 'user_account_register',
+    component: UserAccountRegisterView,
   },
   {
     path: '/404/',
